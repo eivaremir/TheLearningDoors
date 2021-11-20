27cd96701ab4b21b9cc76ad1ab0c017896142f41
Eivar Morales <eivaremir@gmail.com>
2021-11-20 16:01:18 -0500

    Added: grass, stone, dirt

--- game/entities/dirt.py
+++ game/entities/dirt.py
@@ -2,7 +2,7 @@ from ursina import *
 from .voxel import Voxel
 from ..textures import textures
 
-class Grass(Voxel):
+class Dirt(Voxel):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.texture = textures["grass"]
+        self.texture = textures["dirt"]
