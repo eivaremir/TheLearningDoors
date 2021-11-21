6244f4519f2b686ff137f261224c402da1c225f9
Gerardo Fumero <gerardo.fumero@utp.ac.pa>
2021-11-21 01:52:29 -0500

    Songs added to the Sounds directory

---
+++ game/sounds/__init__.py
@@ -0,0 +1,15 @@
+from ursina import *
+
+sounds = {
+    "openDoor" : Audio("../assets/sounds/DoorOpen.mp3"),
+    "closeDoor" : Audio("../assets/sounds/DoorClose.mp3"),       
+    "Error" : Audio("../assets/sounds/Error.mp3"),
+    "GameOver" : Audio("../assets/sounds/Game_Over.mp3"),
+    "GameStart" : Audio("../assets/sounds/Game_Start.mp3"),
+    "GameStart2" : Audio("../assets/sounds/Game_Start2.mp3"),
+    "TimeOut" : Audio("../assets/sounds/Out_of_Time.mp3"),
+    "StepsMinecraft" : Audio("../assets/sounds/Steps_Minecraft.mp3"),
+    "Steps" : Audio("../assets/sounds/Steps.mp3"),
+    "TimeOut2" : Audio("../assets/sounds/Time_Out.mp3"),
+    "Done1" : Audio("../assets/sounds/Well_Done.mp3"),
+}
